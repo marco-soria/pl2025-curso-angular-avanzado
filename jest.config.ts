c9bafa40d9d39454deda859fa2656234500bd846
marco-soria <marco.soriaa7@gmail.com>
2025-10-26 20:55:30 -0500

    added jest config for testing

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+export default {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@shared/(.*)': '<rootDir>/src/app/domains/shared/$1',
+    '@products/(.*)': '<rootDir>/src/app/domains/products/$1',
+    '@info/(.*)': '<rootDir>/src/app/domains/info/$1',
+    '@env/(.*)': '<rootDir>/src/environments/$1',
+  },
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/test/',
+  ],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+} satisfies Config;
